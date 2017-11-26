b51a398e4fa16b951c04865427e06eeb258d05e5
Taiwo Tejuosho <theycallmeflowz@yahoo.com>
2017-11-26 13:45:43 -0600

    api-routes

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === "production") {
 mongoose.Promise = global.Promise;
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/boox",
+  process.env.MONGODB_URI || "mongodb://localhost/libiq",
   {
     useMongoClient: true
   }
