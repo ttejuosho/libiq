f2b9d128ecc53e85142049ab658ca5fc81b3d6e5
Taiwo Tejuosho <theycallmeflowz@yahoo.com>
2017-12-01 10:46:42 -0600

    update serviceworker

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom';
 import './index.css';
-// import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 import routes from "./config/routes";
 
 
 
 ReactDOM.render(routes, document.getElementById('root'));
-// registerServiceWorker();
+registerServiceWorker();
