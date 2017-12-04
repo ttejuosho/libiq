ad04b5c0e4b8af47f263040860f801ff672aae22
Zaynaib Giwa <zaynaibg@gmail.com>
2017-12-04 14:01:09 -0600

    fixed routes on client side code

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,7 @@
 import axios from "axios";
+import moment from 'moment';
 
+window.moment = moment;
 
 export default {
     // Creates new User Object
@@ -18,21 +20,43 @@ export default {
     },
 
     //retrieves title for every book from that user
-    findAllBooks: userId=>{
-        return axios.get(`/api/user/${userId}`/books)
+    findAllBooks: userId =>{
+        return axios.get(`/api/user/${userId}/books`)
             .then(
                 res =>{
-                    return res.data.dueBooksId.title
+                    let bookTitles =[];
+                    let userBooks = res.data.dueBooksId;
+                    for(var i; i<userBooks.length;i++){
+                        bookTitles.push(userBooks.title)
+                    }
+                    return bookTitles
                 }
             )
     },
-
-    // finds the user's favorite book
+    
+    //calculate when the books are due 
+    getBooks: (userId) => {        
+        // API call right here
+        axios.get(`/api/user/${userId}/books`)
+          .then(res => {
+            let books = [];
+            res.data.forEach(book => {
+              let result = {
+                title: book.title,
+                due: moment(book.date).from(moment())
+              };
+              books.push(result);
+            });
+            
+          });
+    
+      },    
+    // finds the user's favorite books
     favBooks: userId => {
         return axios.get(`/api/user/${userId}/books`)
             .then(res =>{
-                favBooks = [];
-                books = res.data.dueBooksId
+                let favBooks = [];
+                let books = res.data.dueBooksId
                 for(var i = 0 ; i< books.length;i++){
                     if(books.isFavorite === true){
                         favBooks.push(books.title)
@@ -41,7 +65,8 @@ export default {
                 return favBooks;
                 
             } )
-    }
+         }
+
 
 
     //creates a user habit
