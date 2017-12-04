d354e2b5fcc502677bc71f939bcce07e882c5bb9
Zaynaib Giwa <zaynaibg@gmail.com>
2017-12-04 13:24:51 -0600

    server routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,24 +2,50 @@ import axios from "axios";
 
 
 export default {
-    // Creates newUSer Object
-    create: dbUser => {
-
+    // Creates new User Object
+    createUser: dbUser => {
         return axios.post(`/api/user`, dbUser);
     },
 
-    // Gets user info object
-    findById: dbUser => {
-        // return axios.get(`/api/user/${userId}`);
+    // Gets user info about the User
+    findUserById: userId => {
+        return axios.get(`/api/user/${userId}`);
     },
 
     // Creates New Book Object
     createBooks: dbBook => {
-        return axios.post(`/api/book/create`, dbBook);
+        return axios.post(`/api/books/`, dbBook);
     },
 
-    // Finds All books Added by a User
-    findAll: dbBook => {
-        // return axios.get(`/api/user/${userId}/book`);
+    //retrieves title for every book from that user
+    findAllBooks: userId=>{
+        return axios.get(`/api/user/${userId}`/books)
+            .then(
+                res =>{
+                    return res.data.dueBooksId.title
+                }
+            )
+    },
+
+    // finds the user's favorite book
+    favBooks: userId => {
+        return axios.get(`/api/user/${userId}/books`)
+            .then(res =>{
+                favBooks = [];
+                books = res.data.dueBooksId
+                for(var i = 0 ; i< books.length;i++){
+                    if(books.isFavorite === true){
+                        favBooks.push(books.title)
+                    }
+                }
+                return favBooks;
+                
+            } )
     }
+
+
+    //creates a user habit
+
+    //finds the habit of the user
+
 };
